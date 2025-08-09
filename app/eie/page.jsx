42f34131829ec399fd980f3485d34984c983753d
Run <165893187+nullrun123@users.noreply.github.com>
2025-08-09 11:56:54 +0700

    pineiei

--- app/eie/page.jsx
+++ app/eie/page.jsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import React from 'react'
 import pine from "@/public/pineapple_full.svg";
+import styles from "@/app/styles/MyBlogs.module.css";
 const eiei = () => {
   return (
     <div className='section'>
         
-        <Image src={pine} width={1000} height={1000} alt="pineapple"></Image>
+        <Image className={styles.pineiei}src={pine} width={1000} height={1000} alt="pineapple"></Image>
     </div>
   )
 }
